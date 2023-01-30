b25e12f7aeb68b0896afaec8afa06d430d8adef3
FallbackColt <1804075821@qq.com>
2023-01-30 23:38:21 +0800

    test

---
+++ spring5_demo/src/main/java/com/test/spring5/User.java
@@ -0,0 +1,11 @@
+package com.test.spring5;
+
+/**
+ * @author: 18040
+ * @date: 2023/1/30 23:24
+ */
+public class User {
+    public void add() {
+        System.out.println("add......");
+    }
+}
